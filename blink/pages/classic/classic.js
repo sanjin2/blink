929ef28cf8c37b62248892df67480fe098dcdaa3
sanjin2 <1437890879@qq.com>
2018-08-26 23:28:39 +0800

    add
    add

--- blink/pages/classic/classic.js
+++ blink/pages/classic/classic.js
@@ -1,18 +1,41 @@
-// pages/classic/classic.js
+import {ClassicModel} from '../../models/classic.js'
+let classic = new ClassicModel()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    test: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    classic.getLatest((res)=>{
+      console.log(res);
+      this.setData({
+        classic:res
+      })
+      // console.log(data);
+    })
+    // http.request({
+    //   url:'classic/latest',
+    //   success:(res)=>{
+    //     console.log(res)
+    //   }
+    // })
+    // wx.request({
+    //   url: 'http://bl.7yue.pro/v1/classic/latest',
+    //   header:{
+    //     appkey:"98HcsgdJ3mx4Ufcm"
+    //   },
+    //   success:(res)=>{
+    //     console.log(res);
+    //   }
+    // })
   },
 
   /**
